a57e6d85274b7e6959658c00aca108c02a51130f
molexandev01~ <molexandev@gmail.com>
2024-11-30 22:18:56 +0200

    added several methods to the class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 // import HelloWoeldComponent from './HelloWorldComponent';
-// import MyClassComponent from './MyClassComponent';
-// import { useState } from 'react';
+import MyClassComponent from './MyClassComponent';
+import { useState } from 'react';
 // import CounterComponent from './CounterComponent';
 import ListComponent from './ListComponent';
 // import RenderComponent from './RenderComponent';
 import './App.css';
+// import { useState } from 'react';
 
 function App() {
+   const [isShowTimer, setisShowTimer] = useState(false);
+
    return (
       <div className="App">
          <header className="App-header">
-            {/* {<HelloWoeldComponent />}
-            {<MyClassComponent />} */}
-            <ListComponent />
+            {isShowTimer ? <MyClassComponent /> : <ListComponent />}
+            <button onClick={() => setisShowTimer((prev) => !prev)}>
+               Show timer
+            </button>
+            {/* {<HelloWoeldComponent />} */}
+            {/* {<MyClassComponent />} */}
+            {/* <ListComponent /> */}
             {/* <CounterComponent /> */}
             {/* <RenderComponent /> */}
          </header>
--- src/MyClassComponent.js
+++ src/MyClassComponent.js
@@ -1,15 +1,56 @@
 import { Component } from 'react';
 
 class MyClassComponent extends Component {
-   constructor(props) {
-      super(props);
-      this.state = { name: 'Olexandr' };
+   state = {
+      todos: [],
+      input: '',
+      timer: 0,
+   };
+
+   componentDidMount() {
+      this.intervalId = setInterval(() => {
+         this.setState((prevState) => ({ timer: prevState.timer + 1 }));
+      }, 1000);
+
+      const lsTodos = localStorage.getItem('todos');
+      if (lsTodos) {
+         this.setState({ todos: JSON.parse(lsTodos) });
+      }
+      console.log('componentDidMount');
+   }
+
+   componentDidUpdate(prevProps, prevState) {
+      if (prevState.todos !== this.state.todos) {
+         console.log('componentDidUpdate');
+         localStorage.setItem('todos', JSON.stringify(this.state.todos));
+      }
+   }
+
+   addTask = () => {
+      this.setState({ todos: [...this.state.todos, this.state.input] });
+      this.setState({ input: '' });
+   };
+
+   onChangeHandler = (e) => {
+      const value = e.target.value;
+      this.setState({ input: value });
+   };
+
+   componentWillUnmount() {
+      clearInterval(this.intervalId);
    }
 
    render() {
       return (
          <div>
-            <h1>Hello {this.state.name}</h1>
+            <h1>Hello</h1>
+            <h2>{this.state.timer}</h2>
+            <input value={this.state.input} onChange={this.onChangeHandler} />
+            <br />
+            <button onClick={this.addTask}>Add task</button>
+            {this.state.todos.map((todo, index) => (
+               <p key={index}>{todo}</p>
+            ))}
          </div>
       );
    }
